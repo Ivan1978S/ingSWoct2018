15dd50a7be55e86e79919a64e7421f2ba1bed037
Ivan1978S <46537812+Ivan1978S@users.noreply.github.com>
2019-01-09 13:44:05 -0500

    FIBONACCI SERIE

---
+++ fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ivan
+ */
+public class Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner sc = new Scanner(System.in);
+        int numero,fibo1,fibo2,i;
+        do{
+            System.out.print("Introduce numero mayor que 1: ");
+            numero = sc.nextInt();
+        }while(numero<=1);
+        System.out.println("Los " + numero + " primeros términos de la serie de Fibonacci son:"); 
+
+        fibo1=1;
+        fibo2=1; 
+
+        System.out.print(fibo1 + " ");
+        for(i=2;i<=numero;i++){
+             System.out.print(fibo2 + " ");
+             fibo2 = fibo1 + fibo2;
+             fibo1 = fibo2 - fibo1;
+        }
+        System.out.println();
+    }
+}
+    
+
